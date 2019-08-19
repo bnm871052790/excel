0f7de609e8705a6067e9dfc36afbcd742e10c766
majian <871052790@qq.com>
2019-08-19 23:48:29 +0800

    demo

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,264 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="file-content">
+      <input
+        type="file"
+        id="excel-file"
+        accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+      />
     </div>
-    <router-view/>
+    <el-card
+      shadow="never"
+      style="margin: 10px 0;"
+    >
+      <el-form
+        ref="form"
+        :model="form"
+        :inline="true"
+        label-width="80px"
+      >
+        <el-form-item label="运单号">
+          <el-input
+            v-model="form.num"
+            @keyup.enter.native="onSubmit"
+          ></el-input>
+          <el-input style="display: none;"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            type="primary"
+            @click="onSubmit"
+          >查询</el-button>
+          <el-button
+            type="primary"
+            @click="exportExcel"
+          >导出</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <el-row>
+      <el-col :span="11">
+        <el-table
+          :data="tableData"
+          height="450"
+          border
+          style="width: 100%"
+        >
+          <el-table-column
+            prop="客户"
+            label="客户"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="批次号"
+            label="批次号"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="运单号"
+            label="运单号"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="品名"
+            label="品名"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="箱规"
+            label="箱规"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="备注"
+            label="备注"
+          >
+          </el-table-column>
+        </el-table>
+      </el-col>
+      <el-col :span="2"></el-col>
+      <el-col :span="11" id="exportExcel">
+        <el-table
+          :data="selectData"
+          style="width: 100%"
+          height="450"
+        >
+          <el-table-column label="扫码数据统计">
+            <el-table-column :label="`导入数据：${tableLengthData.tableLength}票;实际扫码：${tableLengthData.actual}票;数据内：${tableLengthData.inData};不在数据内：${tableLengthData.noInData}`">
+              <el-table-column label="未扫码运单号">
+                <el-table-column
+                  prop="客户"
+                  label="客户"
+                >
+                </el-table-column>
+                <el-table-column
+                  prop="批次号"
+                  label="批次号"
+                >
+                </el-table-column>
+                <el-table-column
+                  prop="运单号"
+                  label="运单号"
+                >
+                </el-table-column>
+                <el-table-column
+                  prop="品名"
+                  label="品名"
+                >
+                </el-table-column>
+                <el-table-column
+                  prop="箱规"
+                  label="箱规"
+                >
+                </el-table-column>
+                <el-table-column
+                  prop="备注"
+                  label="备注"
+                >
+                </el-table-column>
+              </el-table-column>
+            </el-table-column>
+          </el-table-column>
+        </el-table>
+        <el-table
+          :data="noSelectData"
+          style="width: 100%"
+          height="450"
+        >
+          <el-table-column label="不在数据内分运单">
+            <el-table-column
+              prop="客户"
+              label="客户"
+            >
+            </el-table-column>
+            <el-table-column
+              prop="批次号"
+              label="批次号"
+            >
+            </el-table-column>
+            <el-table-column
+              prop="运单号"
+              label="运单号"
+            >
+            </el-table-column>
+            <el-table-column
+              prop="品名"
+              label="品名"
+            >
+            </el-table-column>
+            <el-table-column
+              prop="箱规"
+              label="箱规"
+            >
+            </el-table-column>
+            <el-table-column
+              prop="备注"
+              label="备注"
+            >
+            </el-table-column>
+          </el-table-column>
+        </el-table>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
+<script>
+import { table2excel } from '@/utils/exportExcel'
+export default {
+  name: 'App',
+  data () {
+    return {
+      tableData: [],
+      selectData: [],
+      noSelectData: [],
+      tableLengthData: {
+        tableLength: 0, // 总票数
+        actual: 0, // 实际扫码
+        inData: 0, // 数据内
+        noInData: 0 // 不在数据内
+      },
+      form: {
+        num: ''
+      }
+    }
+  },
+  mounted () {
+    this.excelFileChange()
+  },
+  methods: {
+    exportExcel () {
+      table2excel('exportExcel')
+    },
+    onSubmit () {
+      if (!this.tableData.length) return this.$message.error('请导入表格')
+      this.tableLengthData.actual = this.tableLengthData.actual + 1
+      for (let i = 0; i < this.tableData.length; i++) {
+        // 查找成功
+        if (this.tableData[i]['运单号'] === this.form.num) {
+          this.tableLengthData.inData = this.tableLengthData.inData + 1
+          const index = this.selectData.findIndex(item => {
+            return item['运单号'] === this.form.num
+          })
+          this.selectData.splice(index, 1)
+          this.$message.success('订单号查询成功')
+          this.form.num = ''
+          return
+        }
+      }
+      // 查询不到
+      this.noSelectData.push({ '运单号': this.form.num })
+      this.tableLengthData.noInData = this.tableLengthData.noInData + 1
+      this.$message.warning('查询不到订单号')
+      this.form.num = ''
+    },
+    excelFileChange () {
+      document.getElementById('excel-file').onchange = (e) => {
+        var files = e.target.files
+        var fileReader = new FileReader()
+        fileReader.onload = (ev) => {
+          try {
+            var data = ev.target.result
+            // eslint-disable-next-line no-undef
+            var workbook = XLSX.read(data, {
+              type: 'binary'
+            }) // 以二进制流方式读取得到整份excel表格对象
+            var persons = [] // 存储获取到的数据
+            console.log(workbook)
+          } catch (e) {
+            console.log('文件类型不正确')
+            return
+          }
+          // 表格的表格范围，可用于判断表头是否数量是否正确
+          var fromTo = ''
+          // 遍历每张表读取
+          for (var sheet in workbook.Sheets) {
+            if (workbook.Sheets.hasOwnProperty(sheet)) {
+              fromTo = workbook.Sheets[sheet]['!ref']
+              console.log(fromTo)
+              // eslint-disable-next-line no-undef
+              persons = persons.concat(XLSX.utils.sheet_to_json(workbook.Sheets[sheet]))
+              // break; // 如果只取第一张表，就取消注释这行
+            }
+          }
+          const length = persons.length
+          this.$set(this.tableLengthData, 'tableLength', length)
+          this.$set(this, 'tableData', [...persons])
+          this.$set(this, 'selectData', [...persons])
+        }
+        // 以二进制方式打开文件
+        fileReader.readAsBinaryString(files[0])
+      }
+    }
+  }
+}
+</script>
+
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 #nav {
